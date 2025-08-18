d80c92f2f3df4d8182049a4dd4ac934b31ac8baf
Thomas-robotique <tmoscheni78@gmail.com>
2025-08-18 09:46:00 +0200

    Update Nouveau Document texte.md

--- Robitic-arm-project/software/arduino/Nouveau Document texte.md.ino
+++ Robitic-arm-project/software/arduino/Nouveau Document texte.md.ino
@@ -1,45 +1,9 @@
-# Code Arduino
+# Code Arduino - Bras Robotique
 
+Dans ce document, vous retrouverez l'évolution de mes codes Arduino durant le projet.
 
+## Description
+Ce code contrôle un bras robotique à trois servomoteurs. Chaque servo est attaché à une broche spécifique et effectue des mouvements simples. 
 
+## Code
 
-
-
-#include <Servo.h>
-
-// C++ code
-//
-Servo myservo2;
-Servo myservo1;
-Servo myservo;
-
-            
-void setup()
-{
-  Serial.begin(9600);
-   myservo2.attach(4);
-
-myservo1.attach (3) ;
-  myservo.attach(2);
-  pinMode(7,INPUT);
-}
-
-void loop()
-{
-
-
-
-  
-  myservo1.write(180);
-  delay(1000);
-    myservo1.write(90);
-delay(1000);
-  myservo.write(180);
-  delay(1000);
-    myservo.write(90);
-
- 
-  
-
-  
-}
